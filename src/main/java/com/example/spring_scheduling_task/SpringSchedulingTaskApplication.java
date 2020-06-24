78911f5d40af31e8d7f1a971fbb5303f97e15139
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-06-24 18:17:49 +0900

    Initial Commit

---
+++ src/main/java/com/example/spring_scheduling_task/SpringSchedulingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_scheduling_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulingTaskApplication.class, args);
+	}
+
+}
